14ae9b95431c03336682944e173c22652142403a
Walter Leinonen <2270077l@student.gla.ac.uk>
2018-01-23 18:25:02 +0000

    added Add Category functionality - needs improvements

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -33,4 +34,22 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context_dict)
 
+def add_category(request):
+    form = CategoryForm()
+    #this view function can handle three scenarios: 1-show a new, blank form for adding a category
+    #2-save form data provided by the user (and it's good) and rendering the rango index page
+    #3-if there are errors in the form data, redisplay the form with error messages
 
+    #here it gets cool
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            #new category has been saved, and we could give a confirmation message here
+            #but since the most rect category added is on the index page
+            #we can just redirect the user back to the index page
+            return index(request)
+        else:
+            #the form supplied contained errors, just print them to the terminal
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
