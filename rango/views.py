ea2b30ef00b7a925f0fffdebccf4e7d88d76b7a5
Walter Leinonen <2270077l@student.gla.ac.uk>
2018-01-23 15:49:32 +0000

    added models to index view, altered index template

--- rango/views.py
+++ rango/views.py
@@ -3,9 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 def index(request):
-    context_dict = {'boldmessage':"Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+#    context_dict = {'boldmessage':"Crunchy, creamy, cookie, candy, cupcake!"}
     #20.1.2018-start playing around with templates
     return render(request, 'rango/index.html', context=context_dict)
     #return HttpResponse("Rango says ayyy<br><a href=\"/rango/about\">link</a>")
