fee76831c151e33e4c8ed9dc41f3ca7a97ee8bf8
Walter Leinonen <2270077l@student.gla.ac.uk>
2018-01-23 17:20:24 +0000

    end of chapter 6

--- rango/views.py
+++ rango/views.py
@@ -7,7 +7,8 @@ from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    pages_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages':pages_list}
 #    context_dict = {'boldmessage':"Crunchy, creamy, cookie, candy, cupcake!"}
     #20.1.2018-start playing around with templates
     return render(request, 'rango/index.html', context=context_dict)
@@ -17,14 +18,14 @@ def about(request):
     context_dict = {'boldmessage':"\"Here\'s the about page!\""}
     return render(request, 'rango/about.html', context=context_dict)
 
-def show_category(request, category_name_url):
+def show_category(request, category_name_slug):
     context_dict = {}
     try:
         # can we find a category name slug matching?
         # get() raises DoesNotExist exception if not
         # get() returns one model instance or raises exception
         category = Category.objects.get(slug=category_name_slug)
-        pages = Pages.objects.filter(category=category)
+        pages = Page.objects.filter(category=category)
         context_dict['pages'] = pages
         context_dict['category'] = category
     except Category.DoesNotExist:
